a673c503b371bdbb1160e62be9f64fbed793dbda
apn999917 <31684381+apn999917@users.noreply.github.com>
2018-08-22 10:11:38 +0700

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -1,7 +1,7 @@
 <?php
 require('./db/connect-db.php'); //เรียกใช้ไฟล์
 $sql_office="select * from tbl_office ";
-$query_office=mysqli_query($conn,sql_office);
+$query_office=mysqli_query($conn,$sql_office);
 while($obj=mysql_fetch_array($query_office))
 {
 	echo $obj["office_name"]."<br>"; //
