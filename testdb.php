ac3774fab53a55f2313893bafb10c660b056bc73
apn999917 <31684381+apn999917@users.noreply.github.com>
2018-08-22 10:42:34 +0700

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -1,10 +1,10 @@
 <?php
 require('./db/connect-db.php'); //เรียกใช้ไฟล์
-$sql_office="select * from tbl_office ";
+$sql_office="select * from tbl_office where office_name like '%เชียง%'";
 $query_office=mysqli_query($conn,$sql_office);
 while($obj=mysqli_fetch_array($query_office))
 {
-	echo $obj["office"]."  ".$obj["office_name"]."<br>"; //
+	echo "รหัสการไฟฟ้า".$obj["office"]."  "."ชื่อการไฟฟ้า".$obj["office_name"]."<br>"; //
 }
 
 ?>
