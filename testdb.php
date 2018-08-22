51e1cc65df800f5020f168733478e3b4fa980849
apn999917 <31684381+apn999917@users.noreply.github.com>
2018-08-22 10:35:25 +0700

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -4,7 +4,7 @@ $sql_office="select * from tbl_office ";
 $query_office=mysqli_query($conn,$sql_office);
 while($obj=mysqli_fetch_array($query_office))
 {
-	echo $obj["office"]."".$obj["office_name"]."<br>"; //
+	echo $obj["office"]."  ".$obj["office_name"]."<br>"; //
 }
 
 ?>
