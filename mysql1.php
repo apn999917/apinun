ec7e874795661ba5f6200cdbf5fd7650ef2c1211
apn999917 <31684381+apn999917@users.noreply.github.com>
2018-10-09 14:14:01 +0700

    Add files via upload

--- mysql1.php
+++ mysql1.php
@@ -1,7 +1,7 @@
 <?php
-  $server = "localhost";
-    $username = "nutticom_data";
-    $password = "12345";
+  $server = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "b798786b8aa714";
+    $password = "2e0e0451";
     $db = "heroku_ce52199dd4f50e1";
     $conn = new mysqli($server, $username, $password, $db);
     // Check connection
