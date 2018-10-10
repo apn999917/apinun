a86bb00d5dfee974292ee1c89d0b6a3b1a2e9e04
apn999917 <31684381+apn999917@users.noreply.github.com>
2018-10-10 09:31:39 +0700

    Add files via upload

--- insert1.php
+++ insert1.php
@@ -12,7 +12,7 @@
 	<body>
 		<div class="container-fluid" style="background-color:pink;">
 			<div class="row row-center">
-				<div class="col-lg-4 offset-lg-4" style="background-color:blue;">
+				<div class="col-lg-1 offset-lg-4" style="background-color:blue;">
 					<h4>บันทึกข้อมูล</h4>
 				</div>
 			</div>
